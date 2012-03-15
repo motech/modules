rajrmen/motech

Duck | #1278 | Made some changes because of corresponding changes in schedule-tracking API.

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/service/impl/EnrollmentDefaultmentService.java
@@ -33,7 +33,7 @@ public class EnrollmentDefaultmentService {
         if (currentMilestone == null)
             return;
 
-        LocalDate currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(schedule.getFirstMilestone().getName());
+        LocalDate currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(schedule.getFirstMilestone().getName(), schedule.isBasedOnAbsoluteWindows());
         LocalDate milestoneEndDate = currentMilestoneStartDate.plus(currentMilestone.getMaximumDuration());
 
         if (milestoneEndDate.isBefore(DateUtil.today()))

motech/modules

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/service/impl/EnrollmentDefaultmentService.java
@@ -32,8 +32,8 @@ public class EnrollmentDefaultmentService {
         if (currentMilestone == null)
             return;
 
-        DateTime currentMilestoneStartDateTime = getCurrentMilestoneStartDate(enrollment);
-        DateTime milestoneEndDateTime = currentMilestoneStartDateTime.plus(currentMilestone.getMaximumDuration());
+        DateTime currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(schedule.getFirstMilestone().getName(), schedule.isBasedOnAbsoluteWindows());
+        DateTime milestoneEndDateTime = currentMilestoneStartDate.plus(currentMilestone.getMaximumDuration());
 
         if (milestoneEndDateTime.isBefore(now()))
             return;