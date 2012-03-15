rajrmen/motech

Duck | #1278 | Introduced concept of schedule whose alerts and windows are always based on reference date.

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/domain/Enrollment.java
@@ -121,8 +121,8 @@ public class Enrollment extends MotechBaseDataObject {
         this.type = type;
     }
 
-    public LocalDate getCurrentMilestoneStartDate(String firstMilestoneNameFromSchedule) {
-        if (currentMilestoneName.equals(firstMilestoneNameFromSchedule))
+    public LocalDate getCurrentMilestoneStartDate(String firstMilestoneNameFromSchedule, boolean scheduleIsBasedOnAbsoluteWindows) {
+        if (currentMilestoneName.equals(firstMilestoneNameFromSchedule) || scheduleIsBasedOnAbsoluteWindows)
             return referenceDate;
         return (fulfillments.isEmpty()) ? enrollmentDate : lastFulfilledDate();
     }

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/service/impl/EnrollmentAlertService.java
@@ -2,7 +2,6 @@ package org.motechproject.scheduletracking.api.service.impl;
 
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
-import org.joda.time.Period;
 import org.motechproject.model.MotechEvent;
 import org.motechproject.model.RepeatingSchedulableJob;
 import org.motechproject.scheduler.MotechSchedulerService;
@@ -34,7 +33,7 @@ public class EnrollmentAlertService {
             return;
 
         String firstMilestoneName = schedule.getFirstMilestone().getName();
-        LocalDate currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(firstMilestoneName);
+        LocalDate currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(firstMilestoneName, schedule.isBasedOnAbsoluteWindows());
         for (MilestoneWindow milestoneWindow : currentMilestone.getMilestoneWindows()) {
             if (currentMilestone.windowElapsed(milestoneWindow.getName(), currentMilestoneStartDate))
                 continue;

motech/modules

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/domain/Enrollment.java
@@ -122,4 +122,10 @@ public class Enrollment extends MotechBaseDataObject {
     public void fulfillCurrentMilestone(DateTime fulfillmentDateTime) {
         fulfillments.add(new MilestoneFulfillment(currentMilestoneName, fulfillmentDateTime));
     }
+
+    public DateTime getCurrentMilestoneStartDate(String firstMilestoneNameFromSchedule, boolean scheduleIsBasedOnAbsoluteWindows) {
+        if (currentMilestoneName.equals(firstMilestoneNameFromSchedule) || scheduleIsBasedOnAbsoluteWindows)
+            return referenceDateTime;
+        return (fulfillments.isEmpty()) ? enrollmentDateTime : getLastFulfilledDate();
+    }
 }

motech-scheduletracking-api/src/main/java/org/motechproject/scheduletracking/api/service/impl/EnrollmentAlertService.java
@@ -31,7 +31,8 @@ public class EnrollmentAlertService {
         if (currentMilestone == null)
             return;
 
-        DateTime currentMilestoneStartDate = getCurrentMilestoneStartDate(enrollment);
+        String firstMilestoneName = schedule.getFirstMilestone().getName();
+        DateTime currentMilestoneStartDate = enrollment.getCurrentMilestoneStartDate(firstMilestoneName, schedule.isBasedOnAbsoluteWindows());
         for (MilestoneWindow milestoneWindow : currentMilestone.getMilestoneWindows()) {
             if (currentMilestone.windowElapsed(milestoneWindow.getName(), currentMilestoneStartDate))
                 continue;