7dad6a7fa578bbe38a78ae86039dd676ba653ca5
agrzywinski <agrzywinski@soldevelo.com>
2013-08-19 08:41:49 +0200

    Persist Commcare and update Commcare Schema
    Event is sent when the connection with Commcare is started or
    receive a schema change notification from Commcare. Then, the handler
    overwrites data in the new repository.
    
    https://trello.com/c/icY2a41b
    
    Change-Id: I53a7089c01cadef19d99882bf1b21be5d09760be

--- modules/commcare/api/src/main/java/org/motechproject/commcare/web/SchemaController.java
+++ modules/commcare/api/src/main/java/org/motechproject/commcare/web/SchemaController.java
@@ -1,14 +1,16 @@
 package org.motechproject.commcare.web;
 
 import org.motechproject.commcare.domain.CaseInfo;
+import org.motechproject.commcare.domain.CommcareApplication;
 import org.motechproject.commcare.domain.CommcareApplicationJson;
-import org.motechproject.commcare.service.CommcareAppStructureService;
+import org.motechproject.commcare.repository.AllCommcareApplications;
 import org.motechproject.commcare.service.CommcareCaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,7 +20,7 @@ import java.util.List;
 public class SchemaController {
 
     @Autowired
-    private CommcareAppStructureService appStructureService;
+    private AllCommcareApplications allCommcareApplications;
 
     @Autowired
     private CommcareCaseService caseService;
@@ -26,7 +28,12 @@ public class SchemaController {
     @RequestMapping(value = "/schema")
     @ResponseBody
     public List<CommcareApplicationJson> schema() {
-        return appStructureService.getAllApplications();
+        List<CommcareApplication> commcareApplicationsList = allCommcareApplications.getAll();
+        List<CommcareApplicationJson> commcareApplicationJsonList = new ArrayList<>();
+        for (CommcareApplication commcareApplication : commcareApplicationsList) {
+            commcareApplicationJsonList.add(new CommcareApplicationJson(commcareApplication.getApplicationName(), commcareApplication.getResourceUri(), commcareApplication.getModules()));
+        }
+        return commcareApplicationJsonList;
     }
 
     @RequestMapping(value = "/caseList")
