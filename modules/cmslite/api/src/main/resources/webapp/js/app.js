5e420375c6af68e2547fda855646f874546a3ebf
Kamil Knitter <kknitter@soldevelo.com>
2014-01-07 10:53:38 +0100

    BUG: Javascript Error in Tasks
    https://trello.com/c/M1cOBrE4
    
    This fix removes 'bootstrap' module dependencies from
    angular app modules. Required due to removal of
    angular-bootstrap.js:
    http://review.motechproject.org/#/c/3544/
    
    Change-Id: Ifb1714531c0bb11c59b1de6071c15ea9abd1c936

--- modules/cmslite/api/src/main/resources/webapp/js/app.js
+++ modules/cmslite/api/src/main/resources/webapp/js/app.js
@@ -4,7 +4,7 @@
     /* App Module */
 
     angular.module('motech-cmslite', ['motech-dashboard', 'resourceServices',
-                                    'ngCookies', 'bootstrap', 'ngRoute', 'motech-widgets']).config(['$routeProvider',
+                                    'ngCookies', 'ngRoute', 'motech-widgets']).config(['$routeProvider',
         function ($routeProvider) {
             $routeProvider.
                 when('/resources', {templateUrl: '../cmsliteapi/resources/partials/resources.html'}).
