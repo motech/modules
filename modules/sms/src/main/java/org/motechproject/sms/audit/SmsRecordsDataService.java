0ad0e27fadc33ce6d78a3594d0b6b65d8e461eb9
Łukasz Lewczyński <llewczynski@soldevelo.com>
2014-04-25 10:57:38 +0200

    MOTECH-622: Migrate SMS to MDS
    From this commit the SMS module will use MDS and SQL database instead of
    couchdb.
    
    Change-Id: Id12ca95d432176ae0f6c40e4b8338ab57ef12477

---
+++ modules/sms/src/main/java/org/motechproject/sms/audit/SmsRecordsDataService.java
@@ -0,0 +1,29 @@
+package org.motechproject.sms.audit;
+
+import org.joda.time.DateTime;
+import org.motechproject.commons.api.Range;
+import org.motechproject.mds.annotations.Lookup;
+import org.motechproject.mds.annotations.LookupField;
+import org.motechproject.mds.service.MotechDataService;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Used to query the audit records in the database
+ */
+public interface SmsRecordsDataService extends MotechDataService<SmsRecord> {
+
+    @Lookup
+    List<SmsRecord> findByCriteria(@LookupField(name = "config") String config,
+                                   @LookupField(name = "smsDirection") Set<SmsDirection> directions,
+                                   @LookupField(name = "phoneNumber") String phoneNumber,
+                                   @LookupField(name = "messageContent") String messageContent,
+                                   @LookupField(name = "timestamp") Range<DateTime> timestamp,
+                                   @LookupField(name = "deliveryStatus") Set<DeliveryStatus> deliveryStatuses,
+                                   @LookupField(name = "providerStatus") String providerStatus,
+                                   @LookupField(name = "motechId") String motechId,
+                                   @LookupField(name = "providerId") String providerId,
+                                   @LookupField(name = "errorMessage") String errorMessage);
+
+}
