d8f6959460553819d45319b3f76ebabde1ab7430
Koshal Thirumalai <kosh@koshal.org>
2014-06-30 12:03:07 -0700

    Updated domain and service interfaces for review
    Change-Id: Id92751fdf927dd22bb5f35e6dd914a8cc308b137

--- mtraining/src/test/java/org/motechproject/mtraining/service/it/MTrainingServiceIT.java
+++ mtraining/src/test/java/org/motechproject/mtraining/service/it/MTrainingServiceIT.java
@@ -9,27 +9,26 @@ import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.motechproject.testing.osgi.container.MotechNativeTestContainerFactory;
 import org.ops4j.pax.exam.ExamFactory;
-import org.ops4j.pax.exam.spi.reactors.PerClass;
+
 import org.motechproject.testing.osgi.BasePaxIT;
+import org.ops4j.pax.exam.spi.reactors.PerSuite;
 
 import javax.inject.Inject;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
- * Verify that HelloWorldService present, functional.
+ * Verify that mTrainingService present, functional.
  */
 @RunWith(PaxExam.class)
-@ExamReactorStrategy(PerClass.class)
+@ExamReactorStrategy(PerSuite.class)
 @ExamFactory(MotechNativeTestContainerFactory.class)
 public class MTrainingServiceIT extends BasePaxIT {
 
-    @Inject
-    private MTrainingService mtrainingService;
-
     @Test
-    @Ignore
-    public void testmTrainingServiceInstance() throws Exception {
-        assertNotNull(mtrainingService);
+    public void testNull() throws Exception {
+
+        assertNull(null);
     }
 }
