058b22511315e0ef258485aa1e51686aa2095f51
haritha <reddy.haritha@gmail.com>
2014-07-07 17:11:06 +0530

    Execution logs:

---
+++ batch/src/main/java/org/motechproject/batch/dao/BatchExecutionDao.java
@@ -0,0 +1,29 @@
+package org.motechproject.batch.dao;
+
+import java.util.List;
+
+import org.motechproject.batch.model.JobExecutionHistoryDTO;
+import org.motechproject.batch.model.JobExecutionHistoryListDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+@Service
+public class BatchExecutionDao {
+
+    @Autowired
+    private JdbcTemplate jdbcTempalte;
+
+    public JobExecutionHistoryListDTO getExecutionHist(String jobName) {
+        JobExecutionHistoryListDTO jobExecutionHistoryList = new JobExecutionHistoryListDTO();
+        String sql = "select je.* from BATCH_JOB_INSTANCE ji "
+                + "inner join BATCH_JOB_EXECUTION je on ji.JOB_INSTANCE_ID=je.JOB_INSTANCE_ID where ji.JOB_NAME='"
+                + jobName +"'";
+        List<JobExecutionHistoryDTO> jobExecutionHistory = jdbcTempalte.query(
+                sql, new BeanPropertyRowMapper<JobExecutionHistoryDTO>(JobExecutionHistoryDTO.class));
+        jobExecutionHistoryList.setJobExecutionHistoryList(jobExecutionHistory);
+        return jobExecutionHistoryList;
+
+    }
+}
