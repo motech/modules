d547eaef2b81d1a66eeae6a135059ef98aee7713
Rob LaRubbio <rob@larubbio.org>
2014-09-03 13:30:39 -0700

    Add a small script to update motech-version in the pom
    Change-Id: I15f828394346dd8260e1f1c2e899a6adf6c27d5d

---
+++ tools/version_update.py
@@ -0,0 +1,12 @@
+import sys
+from xml.dom.minidom import parse
+
+pom = open("pom.xml")
+dom = parse(pom)
+pom.close()
+mv = dom.getElementsByTagName('motech.version')
+mv[0].childNodes[0].data = sys.argv[1]
+
+f = open("pom.xml", 'w')
+dom.writexml(f)
+f.close()
