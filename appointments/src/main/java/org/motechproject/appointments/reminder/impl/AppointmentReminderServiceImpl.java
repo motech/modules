95f61cfb8b2ad02b06854566697acf682ef47183
Marek Kustusz <mkustusz@soldevelo.com>
2014-11-30 12:29:45 +0100

    MOTECH-1121: Add support for period in Appointment scheduler
    Period is now supported in Scheduler so changed RepeatingSchedulableJob
    to RepeatingPeriodSchedulableJob.
    
    Change-Id: I8ba5f904360240db20a0f3869deb81c8b99aabfe

--- appointments/src/main/java/org/motechproject/appointments/reminder/impl/AppointmentReminderServiceImpl.java
+++ appointments/src/main/java/org/motechproject/appointments/reminder/impl/AppointmentReminderServiceImpl.java
@@ -1,11 +1,10 @@
 package org.motechproject.appointments.reminder.impl;
 
-import org.joda.time.Period;
 import org.motechproject.appointments.domain.Appointment;
 import org.motechproject.appointments.reminder.AppointmentReminderService;
 import org.motechproject.event.MotechEvent;
+import org.motechproject.scheduler.contract.RepeatingPeriodSchedulableJob;
 import org.motechproject.scheduler.service.MotechSchedulerService;
-import org.motechproject.scheduler.contract.RepeatingSchedulableJob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,12 +13,6 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class AppointmentReminderServiceImpl implements AppointmentReminderService {
-
-    private static final int MILLISECONDS = 1000;
-    private static final int SECONDS = 60;
-    private static final int MINUTES = 60;
-    private static final int HOURS = 24;
-    private static final int DAYS = 7;
 
     private static final String SUBJECT = "Appointment.Reminder.%s.%s";
 
@@ -33,8 +26,8 @@ public class AppointmentReminderServiceImpl implements AppointmentReminderServic
     @Override
     public void addReminders(Appointment appointment) {
         if (appointment.getReminderInterval() != null) {
-            RepeatingSchedulableJob reminder = buildJob(appointment);
-            this.schedulerService.safeScheduleRepeatingJob(reminder);
+            RepeatingPeriodSchedulableJob reminder = buildJob(appointment);
+            this.schedulerService.safeScheduleRepeatingPeriodJob(reminder);
         }
     }
 
@@ -50,17 +43,12 @@ public class AppointmentReminderServiceImpl implements AppointmentReminderServic
     /**
      * Helper to create a repeating schedulable job from an appointment
      * @param appointment The appointment object to create the schedulable job for
-     * @return a repeating job for the scheduler
+     * @return a repeating period job for the scheduler
      */
-    private RepeatingSchedulableJob buildJob(Appointment appointment) {
+    private RepeatingPeriodSchedulableJob buildJob(Appointment appointment) {
         String eventTitle = String.format(SUBJECT, appointment.getExternalId(), appointment.getApptId());
         MotechEvent event = new MotechEvent(eventTitle);
 
-        // temp workaround for lack of period support in scheduler, ignoring anything bigger than weeks for now. Tracking MOTECH-1121
-        Period interval = appointment.getReminderInterval();
-        long reminderInterval = interval.getWeeks() * DAYS * HOURS * MINUTES * SECONDS * MILLISECONDS + interval.getDays() * HOURS * MINUTES * SECONDS * MILLISECONDS +
-                interval.getHours() * MINUTES * SECONDS * MILLISECONDS + interval.getMinutes() * SECONDS * MILLISECONDS + interval.getSeconds() * MILLISECONDS + interval.getMillis();
-
-        return new RepeatingSchedulableJob(event, appointment.getReminderStartTime().toDate(), appointment.getAppointmentDate().toDate(), reminderInterval, true);
+        return new RepeatingPeriodSchedulableJob(event, appointment.getReminderStartTime().toDate(), appointment.getAppointmentDate().toDate(), appointment.getReminderInterval(), true);
     }
 }
