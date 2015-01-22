a3cb15a48ed1ce4205d59dfbbb53ce7223508fb3
Lauren <llavoie@grameenfoundation.org>
2015-01-22 10:00:17 -0800

    Update copyright year for 2015 (modules repo)
    Update LICENSE.txt and documentation copyright
    
    Change-Id: Ieaf923304401044c9f2a3d28e68d2950500a4635

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -57,7 +57,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'MOTECH Modules'
-copyright = u'2014, MOTECH Team'
+copyright = u'2015, MOTECH Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
