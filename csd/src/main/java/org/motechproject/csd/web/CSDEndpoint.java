a016d3101782b7e062e40dd41566ad3ae38a16ac
Jakub Kondrat <jkondrat@soldevelo.com>
2015-03-16 16:01:46 +0100

    Adding CSD SOAP Endpoint
    Change-Id: I6f1c8b9c3639306e359e8a6a794a5b1a86d613e5

---
+++ csd/src/main/java/org/motechproject/csd/web/CSDEndpoint.java
@@ -0,0 +1,26 @@
+package org.motechproject.csd.web;
+
+import org.motechproject.csd.domain.GetModificationsRequest;
+import org.motechproject.csd.domain.GetModificationsResponse;
+import org.motechproject.csd.service.CSDService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class CSDEndpoint {
+
+    @Autowired
+    private CSDService csdService;
+
+    @PayloadRoot(namespace = "urn:ihe:iti:csd:2013", localPart = "getModificationsRequest")
+    @ResponsePayload
+    public GetModificationsResponse getModifications(@RequestPayload GetModificationsRequest request) {
+        GetModificationsResponse response = new GetModificationsResponse();
+        response.setCSD(csdService.getCSD());
+
+        return response;
+    }
+}
