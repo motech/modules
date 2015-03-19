afe5d304407f37ab24388b25043de185ffddddb8
Jakub Kondrat <jkondrat@soldevelo.com>
2015-03-19 17:22:10 +0100

    UI improvements, more descriptive errors
    Change-Id: Ic0d9306ec099253628a7a69ee2454aa034aa9975

--- csd/src/main/java/org/motechproject/csd/web/CSDController.java
+++ csd/src/main/java/org/motechproject/csd/web/CSDController.java
@@ -1,8 +1,11 @@
 package org.motechproject.csd.web;
 
+import org.apache.commons.lang.StringUtils;
 import org.motechproject.csd.client.CSDHttpClient;
 import org.motechproject.csd.service.CSDService;
 import org.motechproject.csd.service.ConfigService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -12,11 +15,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.xml.sax.SAXParseException;
 
+import javax.xml.bind.UnmarshalException;
 import java.io.IOException;
 
 @Controller
 public class CSDController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CSDController.class);
 
     private ConfigService configService;
 
@@ -37,22 +44,32 @@ public class CSDController {
         String xmlUrl = configService.getConfig().getXmlUrl();
 
         if (xmlUrl == null) {
-            throw new IllegalArgumentException("Xml url cannot be null");
+            throw new IllegalArgumentException("The CSD Registry URL is empty");
         }
 
         String xml = csdHttpClient.getXml(xmlUrl);
 
         if (xml == null) {
-            throw new IllegalArgumentException("Cannot load xml");
+            throw new IllegalArgumentException("Couldn't load XML");
         }
 
         csdService.saveFromXml(xml);
     }
 
     @ExceptionHandler(Exception.class)
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ResponseBody
     public String handleException(Exception e) throws IOException {
-        return e.getMessage();
+        String message = e.getMessage();
+        LOGGER.error(message, e);
+        if (e.getCause() != null && e.getCause() instanceof UnmarshalException) {
+            UnmarshalException cause = (UnmarshalException) e.getCause();
+            if (cause.getLinkedException() != null && cause.getLinkedException() instanceof SAXParseException) {
+                SAXParseException parseException = (SAXParseException) cause.getLinkedException();
+                message += " -" + StringUtils.substringAfter(parseException.getMessage(), ":") +
+                        " (line " + parseException.getLineNumber() + ")";
+            }
+        }
+        return message;
     }
 }
