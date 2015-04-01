e677d01b80285641557e4cfdf49bbd30b1c71d08
AMAN <aman.jaiswal53@gmail.com>
2015-04-01 17:08:41 +0530

    MOTECH-1527: IVR module should have a "Log" tab for viewing CDRs
    Added a Log Tab which on click redirects to DataBrowser of CallDetailRecords. Right Now records are only shown if DataBrowser module has been loaded atleast once after login. To load Data service by default after login we need to make changes to platform-server.
    
    Change-Id: Iaa96e8049cd808566b6c2946ef6ca48f89524268

--- ivr/src/main/resources/webapp/js/app.js
+++ ivr/src/main/resources/webapp/js/app.js
@@ -1,11 +1,23 @@
 (function () {
     'use strict';
 
-    angular.module('ivr', ['motech-dashboard', 'ivr.controllers', 'ngCookies', 'ui.bootstrap']).config(
+    var ivrmodule = angular.module('ivr', ['motech-dashboard', 'ivr.controllers', 'ngCookies', 'ui.bootstrap']), id;
+
+    $.ajax({
+        url: '../mds/entities/getEntity/IVR Module/CallDetailRecord',
+        success:  function(data) {
+            id = data.id;
+        },
+        async: false
+    });
+
+    ivrmodule.config(
     ['$routeProvider',
         function ($routeProvider) {
             $routeProvider.
                 when('/ivr/templates', {templateUrl: '../ivr/resources/partials/templates.html', controller: 'IvrTemplatesCtrl'}).
-                when('/ivr/settings',  {templateUrl: '../ivr/resources/partials/settings.html',  controller: 'IvrSettingsCtrl'});
+                when('/ivr/settings', {templateUrl: '../ivr/resources/partials/settings.html',  controller: 'IvrSettingsCtrl'}).
+                when('/ivr/logs', {redirectTo: 'mds/dataBrowser/'+id});
+
     }]);
 }());
