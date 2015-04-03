391983ccf0118d8b9fa2fd0efe6513bc0dda1d1b
Krzysztof Kaczmarczyk <kkaczmarczyk@soldevelo.com>
2015-04-03 16:04:20 +0200

    added separate date picker for scheduler start date
    Change-Id: I944cf1eb67cfa8e5018f339b0cdc11d9f7603272

--- csd/src/main/resources/webapp/js/directives.js
+++ csd/src/main/resources/webapp/js/directives.js
@@ -3,7 +3,7 @@
 
     var directives = angular.module('csd.directives', []);
 
-    directives.directive('csdDatePicker', function() {
+    directives.directive('csdLastModifiedDatePicker', function() {
         return {
             restrict: 'A',
             require: 'ngModel',
@@ -25,4 +25,27 @@
         };
     });
 
+    directives.directive('csdSchedulerDatePicker', function() {
+            return {
+                restrict: 'A',
+                require: 'ngModel',
+                link: function(scope, element, attrs) {
+                    var elem = angular.element(element);
+
+                    elem.datetimepicker({
+                        dateFormat: "yy-mm-dd",
+                        changeMonth: true,
+                        changeYear: true,
+                        minDate: 0,
+                        timeFormat: "HH:mm",
+                        minTime: 0,
+                        onSelect: function (selectedDateTime){
+                            scope.$apply(function() {
+                                scope.config.startDate = selectedDateTime;
+                            });
+                        }
+                    });
+                }
+            };
+        });
 }());
