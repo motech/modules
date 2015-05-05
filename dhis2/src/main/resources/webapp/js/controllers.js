5ed727f52a42a154c8a40786f1b257eb2e6f67e2
Scott Kimball <scott.r.kimball@maine.edu>
2015-05-05 14:57:54 -0400

    MOTECH-1601 Read Only Representation of DHIS2 Schema in the DHIS2 Module
    Adds a read only representation of the DHIS2 schema in the DHIS2 module.
    In order to sync with the DHIS2 test server use the following credentials:
    URL : 130.111.132.37:8082
    username: admin
    password: district
    
    Change-Id: Ib39ad431c770056263ef13c549f4b76436a3c2ff

--- dhis2/src/main/resources/webapp/js/controllers.js
+++ dhis2/src/main/resources/webapp/js/controllers.js
@@ -45,5 +45,90 @@
         };
     });
 
+    controllers.controller('Dhis2ProgramsCtrl', function ($scope, Programs) {
+        $scope.formError = false;
+        blockUI();
+
+        $scope.programs = Programs.query(function () {
+            $scope.formError = false;
+
+            angular.forEach($scope.programs, function(program) {
+                program.collapsed = true;
+                program.attributes.collapsed = true;
+                program.stages.collapsed = true;
+
+                angular.forEach(program.stages, function(stage) {
+                   stage.collapsed= true;
+                });
+
+            });
+
+            unblockUI();
+        }, function () {
+            $scope.formError = true;
+            unblockUI();
+        });
+        innerLayout({});
+
+        $scope.collapse = function (item) {
+            item.collapsed = !item.collapsed;
+
+        };
+
+        $scope.getTypeOfProgram = function(program) {
+           if (program.singleEvent) {
+
+               if (program.registration) {
+                   return "dhis2.programType.singleEventWithRegistration"
+
+               } else {
+                   return "dhis2.programType.singleEventWithoutRegistration";
+               }
+
+           } else {
+               return "dhis2.programType.MultipleEventsWithRegistration";
+           }
+        };
+    });
+
+    controllers.controller('Dhis2TrackedEntityAttributesCtrl', function($scope, TrackedEntityAttributes) {
+        blockUI();
+        $scope.trackedEntityAttributes = TrackedEntityAttributes.query(function() {
+            unblockUI();
+        }, function () {
+           unblockUI();
+        });
+        innerLayout({});
+    });
+
+    controllers.controller('Dhis2TrackedEntitiesCtrl', function($scope, TrackedEntities) {
+        blockUI();
+        $scope.trackedEntities = TrackedEntities.query(function() {
+            unblockUI();
+        }, function () {
+            unblockUI();
+        });
+        innerLayout({});
+    });
+
+    controllers.controller('Dhis2OrgUnitsCtrl', function($scope, OrgUnits) {
+        blockUI();
+        $scope.orgUnits = OrgUnits.query(function() {
+            unblockUI();
+        }, function () {
+            unblockUI();
+        });
+        innerLayout({});
+    });
+
+    controllers.controller('Dhis2DataElementsCtrl', function($scope, DataElements) {
+        blockUI();
+        $scope.dataElements = DataElements.query(function() {
+            unblockUI();
+        }, function () {
+            unblockUI();
+        });
+        innerLayout({});
+    });
 
 }());
