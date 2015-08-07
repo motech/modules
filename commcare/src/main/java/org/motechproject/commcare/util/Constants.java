1c50254d86ca49ab60a4bb13dd612f6f1db837db
Nikodem Graczewski <ngraczewski@soldevelo.com>
2015-08-07 12:52:35 +0200

    MOTECH-1700: Javadocs for org.motechproject.commcare.util package
    Added missing javadocs for org.motechproject.commcare.util package.
    
    Change-Id: I07539922cd5300853517c765f7450c5e66c67ff8

--- commcare/src/main/java/org/motechproject/commcare/util/Constants.java
+++ commcare/src/main/java/org/motechproject/commcare/util/Constants.java
@@ -1,10 +1,15 @@
 package org.motechproject.commcare.util;
 
-
+/**
+ * Utility class for storing constants.
+ */
 public final class Constants {
 
     public static final String HAS_MANAGE_COMMCARE_PERMISSION = "hasRole('manageCommcare')";
 
+    /**
+     * Utility class, should not be initiated.
+     */
     private Constants() {
     }
 }
