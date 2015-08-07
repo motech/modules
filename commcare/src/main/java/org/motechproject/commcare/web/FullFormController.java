7a955ca31262557c64f0fa6eda3b3e267b7a855c
Paweł Gesek <pgesek@soldevelo.com>
2015-08-07 16:52:46 +0200

    MOTECH-1829: Separated event logic for reuse
    I created classes representing the form events. These
    can be reused in the new component that will pull the forms.
    I also added helper methods for increasing and decreasing
    the page in FormListRequest.
    
    Change-Id: Ib9e86b330aeeb0dcc52f7710aa4d651e2ed6509a

--- commcare/src/main/java/org/motechproject/commcare/web/FullFormController.java
+++ commcare/src/main/java/org/motechproject/commcare/web/FullFormController.java
@@ -1,16 +1,14 @@
 package org.motechproject.commcare.web;
 
-import com.google.common.collect.LinkedHashMultimap;
-import com.google.common.collect.Multimap;
 import org.motechproject.commcare.config.Config;
 import org.motechproject.commcare.domain.FormValueElement;
+import org.motechproject.commcare.events.FullFormEvent;
+import org.motechproject.commcare.events.FullFormFailureEvent;
 import org.motechproject.commcare.exception.ConfigurationNotFoundException;
 import org.motechproject.commcare.exception.EndpointNotSupported;
 import org.motechproject.commcare.exception.FullFormParserException;
 import org.motechproject.commcare.parser.FullFormParser;
 import org.motechproject.commcare.service.CommcareConfigService;
-import org.motechproject.commcare.service.impl.CommcareFormsEventParser;
-import org.motechproject.commons.api.TasksEventParser;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.EventRelay;
 import org.slf4j.Logger;
@@ -27,18 +25,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-
-import static org.motechproject.commcare.events.constants.EventDataKeys.ATTRIBUTES;
-import static org.motechproject.commcare.events.constants.EventDataKeys.CONFIG_NAME;
-import static org.motechproject.commcare.events.constants.EventDataKeys.ELEMENT_NAME;
-import static org.motechproject.commcare.events.constants.EventDataKeys.FAILED_FORM_MESSAGE;
-import static org.motechproject.commcare.events.constants.EventDataKeys.RECEIVED_ON;
-import static org.motechproject.commcare.events.constants.EventDataKeys.SUB_ELEMENTS;
-import static org.motechproject.commcare.events.constants.EventDataKeys.VALUE;
-import static org.motechproject.commcare.events.constants.EventSubjects.DEVICE_LOG_EVENT;
-import static org.motechproject.commcare.events.constants.EventSubjects.FORMS_EVENT;
-import static org.motechproject.commcare.events.constants.EventSubjects.FORMS_FAIL_EVENT;
-import static org.motechproject.commcare.parser.FullFormParser.FORM;
 
 /**
  * Controller that handles the incoming full form feed from CommCareHQ. The path to this endpoint
@@ -87,33 +73,6 @@ public class FullFormController {
         return e.getMessage();
     }
 
-    private void publishMalformedFormMessage(String message) {
-        Map<String, Object> params = new HashMap<>();
-        params.put("message", "Received malformed form: " + message);
-        params.put("level", "CRITICAL");
-        params.put("moduleName", "commcare");
-
-        eventRelay.sendEventMessage(new MotechEvent("org.motechproject.message", params));
-    }
-
-    private Multimap<String, Map<String, Object>> convertToMap(Multimap<String, FormValueElement> subElements) {
-        Multimap<String, Map<String, Object>> elements = LinkedHashMultimap.create();
-
-        for (Map.Entry<String, FormValueElement> entry : subElements.entries()) {
-            Map<String, Object> elementAsMap = new HashMap<>(4); //NO CHECKSTYLE MagicNumber
-            FormValueElement formValueElement = entry.getValue();
-
-            elementAsMap.put(ELEMENT_NAME, formValueElement.getElementName());
-            elementAsMap.put(SUB_ELEMENTS, convertToMap(formValueElement.getSubElements()));
-            elementAsMap.put(ATTRIBUTES, formValueElement.getAttributes());
-            elementAsMap.put(VALUE, formValueElement.getValue());
-
-            elements.put(entry.getKey(), elementAsMap);
-        }
-
-        return elements;
-    }
-
     private String getConfigName(HttpServletRequest request) {
         String pathInfo = request.getPathInfo();
         return pathInfo.substring(pathInfo.lastIndexOf('/') + 1);
@@ -128,32 +87,27 @@ public class FullFormController {
         }
 
         FullFormParser parser = new FullFormParser(body);
-        FormValueElement formValueElement;
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put(CONFIG_NAME, config.getName());
-        parameters.put(TasksEventParser.CUSTOM_PARSER_EVENT_KEY, CommcareFormsEventParser.PARSER_NAME);
 
         try {
-            formValueElement = parser.parse();
+            FormValueElement formValueElement = parser.parse();
 
-            parameters.put(RECEIVED_ON, request.getHeader("received-on"));
-            parameters.put(VALUE, formValueElement.getValue());
-            parameters.put(ELEMENT_NAME, formValueElement.getElementName());
-            parameters.put(ATTRIBUTES, formValueElement.getAttributes());
-            parameters.put(SUB_ELEMENTS, convertToMap(formValueElement.getSubElements()));
+            FullFormEvent fullFormEvent = new FullFormEvent(formValueElement, request.getHeader("received-on"),
+                    config.getName());
 
-            if (FORM.equals(formValueElement.getElementName())) {
-                eventRelay.sendEventMessage(new MotechEvent(FORMS_EVENT, parameters));
-            } else {
-                eventRelay.sendEventMessage(new MotechEvent(DEVICE_LOG_EVENT, parameters));
-            }
+            eventRelay.sendEventMessage(fullFormEvent.toMotechEvent());
         } catch (FullFormParserException e) {
-            MotechEvent failedEvent = new MotechEvent(FORMS_FAIL_EVENT);
-            failedEvent.getParameters().put(CONFIG_NAME, config.getName());
-            failedEvent.getParameters().put(TasksEventParser.CUSTOM_PARSER_EVENT_KEY, CommcareFormsEventParser.PARSER_NAME);
-            failedEvent.getParameters().put(FAILED_FORM_MESSAGE, e.getMessage());
-            eventRelay.sendEventMessage(failedEvent);
+            FullFormFailureEvent failureEvent = new FullFormFailureEvent(config.getName(), e.getMessage());
+            eventRelay.sendEventMessage(failureEvent.toMotechEvent());
             publishMalformedFormMessage(e.getMessage());
         }
+    }
+
+    private void publishMalformedFormMessage(String message) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("message", "Received malformed form: " + message);
+        params.put("level", "CRITICAL");
+        params.put("moduleName", "commcare");
+
+        eventRelay.sendEventMessage(new MotechEvent("org.motechproject.message", params));
     }
 }
