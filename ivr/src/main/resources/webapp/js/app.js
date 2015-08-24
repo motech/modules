63bc1371dfe5d616916b7246df1cd913624de842
Nikodem Graczewski <ngraczewski@soldevelo.com>
2015-08-24 19:02:17 +0200

    MOTECH-1851: Added new permission for displaying logs in the IVR module.
    Change-Id: Ia1b4738edf3e8c8d6d4e3dcbf4d4e46936a76cd8

--- ivr/src/main/resources/webapp/js/app.js
+++ ivr/src/main/resources/webapp/js/app.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    var ivrmodule = angular.module('ivr', ['motech-dashboard', 'ivr.controllers', 'ngCookies', 'ui.bootstrap']), id;
+    var ivrmodule = angular.module('ivr', ['motech-dashboard', 'ivr.controllers', 'ngCookies', 'ui.bootstrap', 'mds']), id;
 
     $.ajax({
         url: '../mds/entities/getEntity/IVR Module/CallDetailRecord',
@@ -11,13 +11,36 @@
         async: false
     });
 
-    ivrmodule.config(
-    ['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider.
-                when('/ivr/templates', {templateUrl: '../ivr/resources/partials/templates.html', controller: 'IvrTemplatesCtrl'}).
-                when('/ivr/settings', {templateUrl: '../ivr/resources/partials/settings.html',  controller: 'IvrSettingsCtrl'}).
-                when('/ivr/logs', {redirectTo: 'mds/dataBrowser/'+id});
+    $.ajax({
+        url: '../ivr/available/ivrTabs',
+        success:  function(data) {
+            ivrmodule.constant('IVR_AVAILABLE_TABS', data);
+        },
+        async: false
+    });
 
-    }]);
+    ivrmodule.run(function ($rootScope, IVR_AVAILABLE_TABS) {
+        $rootScope.IVR_AVAILABLE_TABS = IVR_AVAILABLE_TABS;
+    });
+
+    ivrmodule.config(function ($routeProvider, IVR_AVAILABLE_TABS) {
+
+        var i, tab;
+
+        for (i = 0; i < IVR_AVAILABLE_TABS.length; i = i + 1) {
+
+            tab = IVR_AVAILABLE_TABS[i];
+
+            if (tab === "log") {
+                $routeProvider.when('/ivr/{0}'.format(tab), {redirectTo: 'mds/dataBrowser/'+id});
+            } else {
+                $routeProvider.when('/ivr/{0}'.format(tab),
+                    {
+                        templateUrl: '../ivr/resources/partials/{0}.html'.format(tab),
+                        controller: 'Ivr{0}Ctrl'.format(tab.capitalize())
+                    }
+                );
+            }
+        }
+    });
 }());
