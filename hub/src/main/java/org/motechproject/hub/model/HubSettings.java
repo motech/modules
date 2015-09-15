8103ead911110f14a3aad4e59cca21cf0304fed3
Lukasz Skarzynski <lskarzynski@soldevelo.com>
2015-09-15 15:37:05 +0200

    MOTECH-1704: Javadoc for Hub module - part 1
    Contains javadoc for model, validation and web packages

--- hub/src/main/java/org/motechproject/hub/model/HubSettings.java
+++ hub/src/main/java/org/motechproject/hub/model/HubSettings.java
@@ -5,20 +5,40 @@ import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonMethod;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+/**
+ * Class which stores base url of the hub. Hub is responsible of
+ * notifying subscribers of changes in topics to which they subscribed.
+ */
 @JsonAutoDetect(JsonMethod.NONE)
 public class HubSettings {
 
     @JsonProperty
     private String hubBaseUrl;
 
+    /**
+     * Gets the hub base url.
+     *
+     * @return the hub base url
+     */
     public String getHubBaseUrl() {
         return hubBaseUrl;
     }
 
+    /**
+     * Sets the hub base url.
+     *
+     * @param hubBaseUrl the hub base url to be set
+     */
     public void setHubBaseUrl(String hubBaseUrl) {
         this.hubBaseUrl = hubBaseUrl;
     }
 
+    /**
+     * For now only checks if base url is not blank.
+     * This is not used at the moment.
+     *
+     * @return false if the hub base url is empty, null or whitespace only, true otherwise
+     */
     public boolean canMakeConnection() {
         return StringUtils.isNotBlank(hubBaseUrl);
     }
--- hub/src/main/java/org/motechproject/hub/model/SettingsDTO.java
+++ hub/src/main/java/org/motechproject/hub/model/SettingsDTO.java
@@ -4,20 +4,38 @@ import org.codehaus.jackson.annotate.JsonAutoDetect;
 import org.codehaus.jackson.annotate.JsonMethod;
 import org.codehaus.jackson.annotate.JsonProperty;
 
+/**
+ * Represent a single hub settings. Used by {@link org.motechproject.hub.web.HubController}.
+ */
 @JsonAutoDetect(JsonMethod.NONE)
 public class SettingsDTO {
 
     @JsonProperty
     private HubSettings settings;
 
+    /**
+     * Creates a new instance of <code>SettingsDTO</code> with new
+     * {@link org.motechproject.hub.model.HubSettings} and the hub base url
+     * set to null.
+     */
     public SettingsDTO() {
         this.settings = new HubSettings();
     }
 
+    /**
+     * Gets the hub base url.
+     *
+     * @return the hub base url as a <code>String</code>
+     */
     public String getHubBaseUrl() {
         return settings.getHubBaseUrl();
     }
 
+    /**
+     * Sets the hub base url.
+     *
+     * @param hubBaseUrl the hub base url to be set
+     */
     public void setHubBaseUrl(String hubBaseUrl) {
         this.settings.setHubBaseUrl(hubBaseUrl);
     }
