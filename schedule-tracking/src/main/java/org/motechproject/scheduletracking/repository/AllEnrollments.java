7769f3c58dc82cef0acfe0307053f523ee99f18e
Tomasz Stalka <tstalka@soldevelo.com>
2015-09-16 10:58:25 +0200

    MOTECH-1710: ScheduleTracking javadoc repository package

--- schedule-tracking/src/main/java/org/motechproject/scheduletracking/repository/AllEnrollments.java
+++ schedule-tracking/src/main/java/org/motechproject/scheduletracking/repository/AllEnrollments.java
@@ -10,36 +10,82 @@ import org.springframework.stereotype.Repository;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * Implementation of DAO interface that utilizes a MDS back-end for retrieving instances.
+ */
 @Repository
 public class AllEnrollments {
 
     @Autowired
     private EnrollmentDataService enrollmentDataService;
 
+    /**
+     * Returns all enrollments stored int he database.
+     *
+     * @return the list of the enrollments
+     */
     public List<Enrollment> retrieveAll() {
         return enrollmentDataService.retrieveAll();
     }
 
+    /**
+     * Returns the list of enrollments with the given external id.
+     *
+     * @param externalId the external id
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> findByExternalId(String externalId) {
         return enrollmentDataService.findByExternalId(externalId);
     }
 
+    /**
+     * Returns the list of enrollments with the given current milestone name.
+     *
+     * @param currentMilestone the current milestone name
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> findByCurrentMilestone(String currentMilestone) {
         return enrollmentDataService.findByMilestoneName(currentMilestone);
     }
 
+    /**
+     * Returns the list of enrollments with the given status.
+     *
+     * @param status the enrollment status
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> findByStatus(EnrollmentStatus status) {
         return enrollmentDataService.findByStatus(status);
     }
 
+    /**
+     * Returns these enrollments, that have got a given key-value entry in their metadata.
+     *
+     * @param property the metadata key
+     * @param value the metadata value
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> findByMetadataProperty(String property, String value) {
         return enrollmentDataService.executeQuery(new MapQueryExecution(property, value));
     }
 
+    /**
+     * Returns the list of enrollments with the given schedule names.
+     *
+     * @param scheduleNames the names of the schedule
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> findBySchedule(List<String> scheduleNames) {
         return enrollmentDataService.findByScheduleName(new HashSet<>(scheduleNames));
     }
 
+    /**
+     * Return all enrollments that have been completed during the given time range.
+     *
+     * @param start the start date and time
+     * @param end the end date end time
+     * @return the list of the enrollments which satisfy the search conditions
+     */
     public List<Enrollment> completedDuring(DateTime start, DateTime end) {
         return enrollmentDataService.executeQuery(new MilestoneRangeQueryExecution(start, end));
     }
--- schedule-tracking/src/main/java/org/motechproject/scheduletracking/repository/TrackedSchedulesJsonReader.java
+++ schedule-tracking/src/main/java/org/motechproject/scheduletracking/repository/TrackedSchedulesJsonReader.java
@@ -4,7 +4,26 @@ import org.motechproject.scheduletracking.domain.json.ScheduleRecord;
 
 import java.util.List;
 
+/**
+ * <code>TrackedSchedulesJsonReader</code> interface provide methods for creating
+ * {@link org.motechproject.scheduletracking.domain.json.ScheduleRecord} form json data and json files.
+ */
 public interface TrackedSchedulesJsonReader {
+
+    /**
+     * Builds schedule record from the given json data.
+     *
+     * @param schduleJson the json data with schedule definition
+     * @return the schedule record
+     */
     ScheduleRecord getSchedule(String schduleJson);
+
+    /**
+     * Builds schedules records from the given directory which is in the classpath. Only files with 'json' extension
+     * will be scanned.
+     *
+     * @param definitionsDirectoryName the name of the directory which is in the classpath
+     * @return the list of the the schedule records
+     */
     List<ScheduleRecord> getAllSchedules(String definitionsDirectoryName);
 }
