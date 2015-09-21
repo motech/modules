2b15a3899092450a02c356ec66a2c8b5ab514335
Sebastian Brudzinski <seb.brudzinski@gmail.com>
2015-09-21 15:50:59 +0200

    MOTECH-1706 Javadoc for Message Campaign
    Applied fixes per Pawel's comments.

--- message-campaign/src/main/java/org/motechproject/messagecampaign/scheduler/EndOfCampaignListener.java
+++ message-campaign/src/main/java/org/motechproject/messagecampaign/scheduler/EndOfCampaignListener.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
- * MOTECH listener, that handles the campaign completed events.
+ * MOTECH listener that handles the campaign completed events.
  */
 @Component
 public class EndOfCampaignListener {
