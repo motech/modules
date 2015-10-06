0bd632fdbfe0b3297b253b4ecb6289fd169889b5
Paweł Gesek <pgesek@soldevelo.com>
2015-10-06 13:18:59 +0200

    MOTECH-1936: Added rounding to the display of decimal fields in IVR
    * We round to two decimal points
    * We don't display the decimal part if it is equal 0
    * If the value is not a number, we leave it as is

--- ivr/src/test/java/org/motechproject/ivr/domain/CallDetailRecordTest.java
+++ ivr/src/test/java/org/motechproject/ivr/domain/CallDetailRecordTest.java
@@ -1,7 +1,10 @@
 package org.motechproject.ivr.domain;
 
+import org.apache.commons.beanutils.PropertyUtils;
 import org.junit.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,6 +16,9 @@ import static org.junit.Assert.assertTrue;
  */
 public class CallDetailRecordTest {
 
+    private static final String MESSAGE_PERCENT_LISTENED = "messagePercentListened";
+    private static final String CALL_DURATION = "callDuration";
+
     @Test
     public void shouldSetCallDetailStatus() {
         CallDetailRecord callDetailRecord = new CallDetailRecord();
@@ -23,7 +29,7 @@ public class CallDetailRecordTest {
     @Test
     public void shouldUseCallDetailStatusMapping() {
         CallDetailRecord callDetailRecord = new CallDetailRecord();
-        Map mapping = setUpCallStatusMapping();
+        Map<String, String> mapping = setUpCallStatusMapping();
 
         callDetailRecord.setField("callStatus", "12", mapping);
         assertEquals("Error", callDetailRecord.getCallStatus());
@@ -58,19 +64,53 @@ public class CallDetailRecordTest {
     @Test
     public void shouldSetCallDetailAsProviderExtraData() {
         CallDetailRecord callDetailRecord = new CallDetailRecord();
-        Map mapping = setUpCallStatusMapping();
+        Map<String, String> mapping = setUpCallStatusMapping();
 
         callDetailRecord.setField( "provider-specific-stuff", "specific-value", mapping);
         assertTrue(callDetailRecord.getProviderExtraData().containsKey("provider-specific-stuff"));
         assertEquals("specific-value", callDetailRecord.getProviderExtraData().get("provider-specific-stuff"));
     }
 
-    private Map setUpCallStatusMapping() {
-        Map mapping = new HashMap<String, String>();
+    @Test
+    public void shouldHandleDecimalsProperly() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Map<String, String> mapping = setUpCallStatusMapping();
+        CallDetailRecord cdr = new CallDetailRecord();
+
+        for (String field : Arrays.asList(MESSAGE_PERCENT_LISTENED, CALL_DURATION)) {
+            cdr.setField(field, null, mapping);
+            assertField(cdr, field, null);
+
+            cdr.setField(field, "", mapping);
+            assertField(cdr, field, "");
+
+            cdr.setField(field, "nonNumber", mapping);
+            assertField(cdr, field, "nonNumber");
+
+            cdr.setField(field, "4.247342", mapping);
+            assertField(cdr, field, "4.25");
+
+            cdr.setField(field, "4.00", mapping);
+            assertField(cdr, field, "4");
+
+            cdr.setField(field, "4.10", mapping);
+            assertField(cdr, field, "4.1");
+
+            cdr.setField(field, "100", mapping);
+            assertField(cdr, field, "100");
+        }
+    }
+
+    private Map<String, String> setUpCallStatusMapping() {
+        Map<String, String> mapping = new HashMap<>();
 
         mapping.put("12", "Error");
         mapping.put("1", "Answered");
 
         return mapping;
+    }
+
+    private void assertField(CallDetailRecord cdr, String field, String expectedValue)
+            throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        assertEquals("Field has wrong value: " + field, expectedValue, PropertyUtils.getProperty(cdr, field));
     }
 }
