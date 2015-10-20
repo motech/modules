e0a2fa1e5033f02dab517d025cd6abf6b5b2b4bd
MikolajBasSoldevelo <mbas@soldevelo.com>
2015-10-20 14:24:06 +0200

    MOTECH 1968 - New lookups for CallDetailRecord, migrate type of motechTimestamp

--- ivr/src/main/java/org/motechproject/ivr/repository/CallDetailRecordDataService.java
+++ ivr/src/main/java/org/motechproject/ivr/repository/CallDetailRecordDataService.java
@@ -3,10 +3,12 @@ package org.motechproject.ivr.repository;
 import org.motechproject.ivr.domain.CallDetailRecord;
 import org.motechproject.mds.annotations.Lookup;
 import org.motechproject.mds.annotations.LookupField;
+import org.motechproject.mds.query.QueryParams;
 import org.motechproject.mds.service.MotechDataService;
 
 import java.util.List;
 
+import static org.motechproject.mds.util.Constants.Operators.MATCHES;
 import static org.motechproject.mds.util.Constants.Operators.MATCHES_CASE_INSENSITIVE;
 
 /**
@@ -25,4 +27,24 @@ public interface CallDetailRecordDataService extends MotechDataService<CallDetai
     List<CallDetailRecord> findByProviderCallIdAndCallStatus(
             @LookupField(name = "providerCallId", customOperator = MATCHES_CASE_INSENSITIVE) String providerCallId,
             @LookupField(name = "callStatus", customOperator = MATCHES_CASE_INSENSITIVE) String callStatus);
+
+    @Lookup
+    List<CallDetailRecord> findByExactProviderCallId(@LookupField(name = "providerCallId") String providerCallId);
+
+    @Lookup
+    List<CallDetailRecord> findByExactProviderCallId(@LookupField(name = "providerCallId") String providerCallId, QueryParams queryParams);
+
+    @Lookup
+    List<CallDetailRecord> findByExactCallStatus(@LookupField(name = "callStatus") String callStatus);
+
+    @Lookup
+    List<CallDetailRecord> findByCallStatus(@LookupField(name = "callStatus", customOperator = MATCHES_CASE_INSENSITIVE) String callStatus);
+
+    @Lookup
+    List<CallDetailRecord> findByMotechTimestamp(@LookupField(name = "motechTimestamp", customOperator = MATCHES) String motechTimestamp);
+
+    @Lookup
+    List<CallDetailRecord> findByMotechTimestampAndCallStatus(@LookupField(name = "motechTimestamp", customOperator = MATCHES) String motechTimestamp, @LookupField(name = "callStatus") String callStatus);
+
+
 }
