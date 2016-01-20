a2d4de9ebd06858cb2701ff32bdee62dd22f2b10
Paweł Gesek <pgesek@soldevelo.com>
2016-01-20 13:43:14 +0100

    Disable ScheduleTrackingTaskBundleIT
    MOTECH-2108: was created for fixing it

--- schedule-tracking/src/test/java/org/motechproject/scheduletracking/it/ScheduleTrackingIntegrationTests.java
+++ schedule-tracking/src/test/java/org/motechproject/scheduletracking/it/ScheduleTrackingIntegrationTests.java
@@ -4,10 +4,9 @@ package org.motechproject.scheduletracking.it;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.motechproject.scheduletracking.osgi.ScheduleTrackingBundleIT;
-import org.motechproject.scheduletracking.osgi.ScheduleTrackingTaskBundleIT;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ScheduleTrackingTaskBundleIT.class, AllEnrollmentsBundleIT.class, EnrollmentsSearchBundleIT.class,
+@Suite.SuiteClasses({/*ScheduleTrackingTaskBundleIT.class, */AllEnrollmentsBundleIT.class, EnrollmentsSearchBundleIT.class,
         ScheduleTrackingServiceBundleIT.class, SchedulingFloatingAlertsWithoutPreferredTimeBundleIT.class,
         SchedulingFloatingAlertsWithPreferredTimeBundleIT.class, SchedulingWithoutPreferredTimeBundleIT.class,
         SchedulingWithPreferredTimeBundleIT.class, ScheduleTrackingBundleIT.class})
