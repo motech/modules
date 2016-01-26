cb2b64c934d522f07dfa743279b9624ab1a9d0c0
atish <atish@beehyv.com>
2016-01-26 16:13:00 +0530

    MOTECH-1926: Clean up duplicating datepicker code
    - Wrote common datepicker code for From & To datepickers in commcare modules

--- sms/src/main/resources/webapp/js/directives.js
+++ sms/src/main/resources/webapp/js/directives.js
@@ -20,44 +20,4 @@
             }
         };
     });
-
-    /*smsGridDatePicker in sms module*/
-    widgetModule.directive('smsGridDatePicker', function() {
-        return {
-            restrict: 'A',
-            link: function(scope, element, attrs) {
-                var elem = angular.element(element),
-                    otherDateTextBox = {},
-                    curId = attrs.id,
-                    curIdLength = curId.length,
-                    otherId = '',
-                    isStartDate = false;
-                     
-                if(curId.substr(curIdLength-2,2) === 'To') {
-                    otherId = curId.slice(0,curIdLength - 2) + 'From';
-                }
-                else {
-                    otherId = curId.slice(0,curIdLength - 4) + 'To';
-                    isStartDate = true;
-                }
-                otherDateTextBox = angular.element('#' + otherId);
-
-                elem.datetimepicker({
-                    dateFormat: "yy-mm-dd",
-                    changeMonth: true,
-                    changeYear: true,
-                    maxDate: +0,
-                    timeFormat: "HH:mm:ss",
-                    onSelect: function (selectedDateTime){
-                        if(isStartDate) {
-                            otherDateTextBox.datetimepicker('option', 'minDate', elem.datetimepicker('getDate') );
-                        }
-                        else {
-                            otherDateTextBox.datetimepicker('option', 'maxDate', elem.datetimepicker('getDate') );
-                        }
-                    }
-                });
-            }
-        };
-    });
 }());
