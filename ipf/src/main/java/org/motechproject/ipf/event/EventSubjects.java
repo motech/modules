a0e8f6f525ac5255635e0d1e96f14c8fa7b4792b
Tomasz Stalka <tstalka@soldevelo.com>
2016-02-06 15:09:15 +0100

    IPF simple task and config integration

---
+++ ipf/src/main/java/org/motechproject/ipf/event/EventSubjects.java
@@ -0,0 +1,13 @@
+package org.motechproject.ipf.event;
+
+/**
+ * Event subjects.
+ */
+public final class EventSubjects {
+
+    public static final String BASE_SUBJECT = "org.motechproject.ipf.";
+
+    public static final String SAMPLE_SUBJECT = BASE_SUBJECT + "sample-sub";
+
+    private EventSubjects() { }
+}
---
+++ ipf/src/main/java/org/motechproject/ipf/handler/IPFActionEventHandler.java
@@ -0,0 +1,19 @@
+package org.motechproject.ipf.handler;
+
+import org.motechproject.event.MotechEvent;
+import org.motechproject.event.listener.annotations.MotechListener;
+import org.motechproject.ipf.event.EventSubjects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IPFActionEventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IPFActionEventHandler.class);
+
+    @MotechListener(subjects =  {EventSubjects.SAMPLE_SUBJECT})
+    public void handleIpfTaskAction(MotechEvent event) {
+        LOGGER.info("Event handled");
+    }
+}
