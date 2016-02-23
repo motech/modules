4ca99dc4c2a4b518a18cf15569adf48a8270f3ac
Scott Kimball <scott.r.kimball@maine.edu>
2016-02-23 11:35:38 -0500

    incorporated second round of comments.

--- odk/src/test/java/org/motechproject/odk/it/OdkIntegrationTests.java
+++ odk/src/test/java/org/motechproject/odk/it/OdkIntegrationTests.java
@@ -4,7 +4,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({EventHandlerIT.class, FormControllerIT.class})
+@Suite.SuiteClasses(FormControllerIT.class)
 public class OdkIntegrationTests {
 
 }
