48580df2206f214c3f7f00054792b8dea65d17f9
Nikodem Graczewski <ngraczewski@soldevelo.com>
2016-03-01 17:28:37 +0100

    MOTECH-2115: Added synchronization of Data Sets to the DHIS2 module
    This PR is a base for the upcoming action modification requested in the MOTECH-2115 ticket.

---
+++ dhis2/src/main/java/org/motechproject/dhis2/repository/DataSetDataService.java
@@ -0,0 +1,8 @@
+package org.motechproject.dhis2.repository;
+
+import org.motechproject.dhis2.domain.DataSet;
+import org.motechproject.mds.service.MotechDataService;
+
+public interface DataSetDataService extends MotechDataService<DataSet> {
+
+}
