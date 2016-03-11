6595563753f364a5e7319114ccdf144036a90c15
Paweł Gesek <pgesek@soldevelo.com>
2016-03-11 17:47:30 +0100

    MOTECH-2270: Fixed issue with events by handled 2x times
    The underlying reason is that @MotechListeners cannot be annotated
    @Transactional at the same time. That leads to the listener being
    registered two times - as the regular bean and as the transactional
    proxy from Spring. This would cause issues with Commcare especially,
    where the schema would get downloaded 2 times.
    
    I improved the code a bit (create in Commcare wasn't doing any cleanup
    for example). The doIntransaction from schedule-tracking can get improved
    with better design, but didn't have time to look into that now.

--- ipf/src/main/java/org/motechproject/ipf/handler/IPFActionEventHandler.java
+++ ipf/src/main/java/org/motechproject/ipf/handler/IPFActionEventHandler.java
@@ -1,19 +1,11 @@
 package org.motechproject.ipf.handler;
 
-import groovy.lang.Writable;
-import groovy.text.Template;
-import groovy.text.XmlTemplateEngine;
-import org.apache.commons.lang.ArrayUtils;
 import org.motechproject.event.MotechEvent;
 import org.motechproject.event.listener.annotations.MotechListener;
-import org.motechproject.ipf.domain.IPFRecipient;
+import org.motechproject.ipf.event.EventSubjects;
 import org.motechproject.ipf.exception.RecipientNotFoundException;
 import org.motechproject.ipf.exception.TemplateNotFoundException;
-import org.motechproject.ipf.domain.IPFTemplate;
-import org.motechproject.ipf.event.EventSubjects;
-import org.motechproject.ipf.service.IPFRecipientsService;
-import org.motechproject.ipf.service.IPFTemplateDataService;
-import org.motechproject.ipf.util.Constants;
+import org.motechproject.ipf.handler.helper.IPFActionHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +15,6 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.util.Map;
 
 /**
  * Component which listens for Motech events and sends data to the HL7 recipients.
@@ -36,13 +27,9 @@ import java.util.Map;
 public class IPFActionEventHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(IPFActionEventHandler.class);
-    private static final String TEMPLATE_DATA_FIELD_NAME = "templateData";
 
     @Autowired
-    private IPFTemplateDataService ipfTemplateDataService;
-
-    @Autowired
-    private IPFRecipientsService ipfRecipientsService;
+    private IPFActionHelper ipfActionHelper;
 
     /**
      * Handles an event and sends data to the HL7 recipient. Template data, recipient name and template name are in the
@@ -56,29 +43,9 @@ public class IPFActionEventHandler {
      */
     @MotechListener(subjects =  {EventSubjects.ALL_TEMPLATE_ACTIONS})
     @Transactional
-    public void handleIpfTaskAction(MotechEvent event) throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException {
+    public void handleIpfTaskAction(MotechEvent event) throws ParserConfigurationException, SAXException, IOException,
+            ClassNotFoundException {
         LOGGER.info("Event handled {}", event.getSubject());
-        Map<String, Object> eventData = event.getParameters();
-
-        String templateName = (String) eventData.get(Constants.TEMPLATE_NAME_PARAM);
-        String recipientName = (String) eventData.get(Constants.RECIPIENT_NAME_PARAM);
-
-        IPFTemplate ipfTemplate = ipfTemplateDataService.findByName(templateName);
-        if (ipfTemplate == null) {
-            LOGGER.error("Cannot find {} template", templateName);
-            throw new TemplateNotFoundException(templateName);
-        }
-
-        IPFRecipient ipfRecipient = ipfRecipientsService.getRecipientbyName(recipientName);
-        if (ipfRecipient == null) {
-            LOGGER.error("Cannot find {} recipient", recipientName);
-            throw new RecipientNotFoundException(templateName);
-        }
-
-        Byte[] templateData = (Byte[]) ipfTemplateDataService.getDetachedField(ipfTemplate, TEMPLATE_DATA_FIELD_NAME);
-        XmlTemplateEngine xmlTemplateEngine = new XmlTemplateEngine();
-        Template xmlTemplate = xmlTemplateEngine.createTemplate(new String(ArrayUtils.toPrimitive(templateData)));
-        Writable writable = xmlTemplate.make(event.getParameters());
-        LOGGER.info("Template with name {}:\n{}", ipfTemplate.getTemplateName(), writable.toString());
+        ipfActionHelper.handleAction(event.getParameters());
     }
 }
