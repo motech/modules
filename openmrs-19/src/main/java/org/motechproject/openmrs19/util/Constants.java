9c4383335032e39d3d3830cb1ae14f7e9259fa03
lkukielka <lkukielka@soldevelo.com>
2016-04-28 17:29:40 +0200

    MOTECH-2363 Added Constants

---
+++ openmrs-19/src/main/java/org/motechproject/openmrs19/util/Constants.java
@@ -0,0 +1,9 @@
+package org.motechproject.openmrs19.util;
+
+public final class Constants {
+
+    public static final String CONFIG_CHANGE_EVENT = "configChange";
+
+    private Constants() {
+    }
+}
