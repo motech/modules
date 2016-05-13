df612e154ced015a3279e016d039c2f79ebd0306
Mateusz Kwiatkowski <mkwiatkowski@soldevelo.com>
2016-05-13 11:18:24 +0200

    MOTECH-1956: Add sending XML template - test

--- ihe-interop/src/test/java/org/motechproject/ihe/interop/handler/helper/IHEActionHelperTest.java
+++ ihe-interop/src/test/java/org/motechproject/ihe/interop/handler/helper/IHEActionHelperTest.java
@@ -1,16 +1,18 @@
 package org.motechproject.ihe.interop.handler.helper;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.motechproject.ihe.interop.domain.CdaTemplate;
 import org.motechproject.ihe.interop.domain.HL7Recipient;
 import org.motechproject.ihe.interop.exception.RecipientNotFoundException;
 import org.motechproject.ihe.interop.exception.TemplateNotFoundException;
 import org.motechproject.ihe.interop.service.HL7RecipientsService;
 import org.motechproject.ihe.interop.service.IHETemplateDataService;
+import org.motechproject.ihe.interop.service.IHETemplateService;
 import org.motechproject.ihe.interop.util.Constants;
-import org.motechproject.ihe.interop.domain.CdaTemplate;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -28,6 +30,9 @@ public class IHEActionHelperTest {
 
     @Mock
     private IHETemplateDataService iheTemplateDataService;
+
+    @Mock
+    private IHETemplateService iheTemplateService;
 
     @Mock
     private CdaTemplate cdaTemplate;
@@ -59,6 +64,27 @@ public class IHEActionHelperTest {
         params.put(Constants.RECIPIENT_NAME_PARAM, "sampleRecipient3");
 
         when(iheTemplateDataService.findByName("sampleTemplate3")).thenReturn(cdaTemplate);
+        iheActionHelper.handleAction(params);
+    }
+
+    @Test
+    public void shouldCallSendTemplateMethod() throws IOException, SAXException, ParserConfigurationException, ClassNotFoundException {
+        Map<String, Object> params = new HashMap<>();
+        params.put(Constants.TEMPLATE_NAME_PARAM, "sampleTemplate3");
+        params.put(Constants.RECIPIENT_NAME_PARAM, "sampleRecipient3");
+
+        byte[] bytes = IOUtils.toByteArray(getClass().getResourceAsStream("/empty_template.xml"));
+        Byte[] byteObjects = new Byte[bytes.length];
+        int i = 0;
+        for (byte b : bytes) {
+            byteObjects[i++] = b;
+        }
+
+
+        when(iheTemplateDataService.findByName("sampleTemplate3")).thenReturn(cdaTemplate);
+        when(hl7RecipientsService.getRecipientbyName("sampleRecipient3")).thenReturn(hl7Recipient);
+        when((Byte[]) iheTemplateDataService.getDetachedField(cdaTemplate, "templateData")).thenReturn(byteObjects);
+
         iheActionHelper.handleAction(params);
     }
 }
