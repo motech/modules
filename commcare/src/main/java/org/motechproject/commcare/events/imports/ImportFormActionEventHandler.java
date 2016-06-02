316a847edde5aa4fdd0909d9f0d5c06afad939a5
lkukielka <lkukielka@soldevelo.com>
2016-06-02 11:54:58 +0200

    MOTECH-2559 Commcare tasks, import forms action (#303)
    * MOTECH-2559 Commcare tasks, import forms action
    
    * MOTECH-2559 Fixed error logging
    
    * MOTECH-2559 Fixes after review
    
    * MOTECH-2559 Fixes after review
    
    * MOTECH-2559 Removed star import
    
    * MOTECH-2559 Fixed after review
    
    * MOTECH-2559 Removed not necessary empty lines
    
    * MOTECH-2559 Created separate importer factory for events
    
    * MOTECH-2559 Fixes after review - removed map using
    
    * MOTECH-2559 Changed unit test name
    
    * MOTECH-2559 Fixes after review
    
    * MOTECH-2559 Removed double semicolon and star import

---
+++ commcare/src/main/java/org/motechproject/commcare/events/imports/ImportFormActionEventHandler.java
@@ -0,0 +1,50 @@
+package org.motechproject.commcare.events.imports;
+
+import org.joda.time.DateTime;
+import org.motechproject.commcare.events.constants.EventSubjects;
+import org.motechproject.commcare.pull.CommcareFormImporterImpl;
+import org.motechproject.commcare.pull.CommcareTasksFormImporterFactory;
+import org.motechproject.commons.api.Range;
+import org.motechproject.event.MotechEvent;
+import org.motechproject.event.listener.annotations.MotechListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static org.motechproject.commcare.events.constants.EventDataKeys.END_DATE;
+import static org.motechproject.commcare.events.constants.EventDataKeys.START_DATE;
+
+/**
+ * Class responsible for handling "Import Forms" action in tasks.
+ */
+@Component
+public class ImportFormActionEventHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ImportFormActionEventHandler.class);
+
+    @Autowired
+    private CommcareTasksFormImporterFactory importerFactory;
+
+    /**
+     * Handles the {@code EventSubjects.IMPORT_FORMS} events. This will import commcare forms for specific configuration
+     *
+     * @param event  the event to be handled
+     */
+    @MotechListener(subjects = EventSubjects.IMPORT_FORMS + ".*")
+    public void handleEvent(MotechEvent event) {
+        CommcareFormImporterImpl importer = importerFactory.getCommcareFormImporter();
+
+        String configName = EventSubjects.getConfigName(event.getSubject());
+        DateTime startDate = (DateTime) event.getParameters().get(START_DATE);
+        DateTime endDate = (DateTime) event.getParameters().get(END_DATE);
+        Range<DateTime> dateRange = new Range<>(startDate, endDate);
+
+        int formsToImport = importer.countForImport(dateRange, configName);
+
+        LOGGER.info("{} commcare forms to be imported.", formsToImport);
+
+        importer.startImport(dateRange, configName);
+    }
+
+}
