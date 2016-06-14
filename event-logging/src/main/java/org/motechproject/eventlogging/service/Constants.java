5b3f2e7d358c77d2cf6c2d2ed31e49ab4f85bad9
beehyvajai <ajai@beehyv.com>
2016-06-14 18:27:27 +0530

    Using properties to enable/disable logging to database/file.

---
+++ event-logging/src/main/java/org/motechproject/eventlogging/service/Constants.java
@@ -0,0 +1,14 @@
+package org.motechproject.eventlogging.service;
+
+
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String DB_ENABLED_PROPERTY = "eventlogging.dbenabled";
+    public static final String FILE_ENABLED_PROPERTY = "eventlogging.fileenabled";
+    public static final String LOGFILE_PATH_PROPERTY = "eventlogging.logfilepath";
+
+}
--- event-logging/src/main/java/org/motechproject/eventlogging/service/impl/FileEventLoggingService.java
+++ event-logging/src/main/java/org/motechproject/eventlogging/service/impl/FileEventLoggingService.java
@@ -1,11 +1,21 @@
 package org.motechproject.eventlogging.service.impl;
 
+import org.motechproject.config.SettingsFacade;
 import org.motechproject.event.MotechEvent;
-import org.motechproject.eventlogging.matchers.LoggableEvent;
+import org.motechproject.eventlogging.converter.impl.DefaultFileToLogConverter;
 import org.motechproject.eventlogging.loggers.EventLogger;
 import org.motechproject.eventlogging.loggers.impl.FileEventLogger;
+import org.motechproject.eventlogging.matchers.LoggableEvent;
+import org.motechproject.eventlogging.matchers.MappingsJson;
+import org.motechproject.eventlogging.repository.AllEventMappings;
+import org.motechproject.eventlogging.service.Constants;
 import org.motechproject.eventlogging.service.EventLoggingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -15,9 +25,19 @@ import java.util.Set;
  * Implementation of {@link org.motechproject.eventlogging.service.EventLoggingService}
  * It is used to log events to files.
  */
+@Service
 public class FileEventLoggingService implements EventLoggingService {
 
     private List<FileEventLogger> fileEventLoggers = new ArrayList<>();
+
+    @Autowired
+    private DefaultFileToLogConverter defaultFileToLogConverter;
+
+    @Autowired
+    private AllEventMappings allEventMappings;
+
+    private SettingsFacade settingsFacade;
+
 
     /**
      * Creates an instance of FileEventLoggingService without any loggers.
@@ -36,6 +56,28 @@ public class FileEventLoggingService implements EventLoggingService {
         if (fileEventLoggers != null) {
             this.fileEventLoggers = fileEventLoggers;
         }
+    }
+
+    @Autowired
+    public void setSettingsFacade(@Qualifier("eventLoggingSettings") SettingsFacade settingsFacade) {
+        this.settingsFacade = settingsFacade;
+    }
+
+    @PostConstruct
+    public void configureFileLoggingService() {
+        List<File> loggingFiles = new ArrayList<>();
+        String logFilePath = settingsFacade.getProperty(Constants.LOGFILE_PATH_PROPERTY);
+        loggingFiles.add(new File(logFilePath));
+
+        List<LoggableEvent> loggableEvents = new ArrayList<>();
+        List<MappingsJson> allMappings = allEventMappings.getAllMappings();
+        for (MappingsJson mapping : allMappings) {
+            LoggableEvent event = new LoggableEvent(mapping.getSubjects(), mapping.getFlags());
+            loggableEvents.add(event);
+        }
+
+        FileEventLogger logger = new FileEventLogger(loggableEvents, loggingFiles, defaultFileToLogConverter);
+        fileEventLoggers.add(logger);
     }
 
     @Override
