6f328fc709d7405862278b65b60debdc1f39d55b
mkruszynski <mkruszynski@soldevelo.com>
2016-06-15 13:55:16 +0200

    MOTECH-2573: Added basic auth to IHE (#310)
    * MOTECH-1734: Removed dropdown, looking only for http(s), replacing other protocols to http
    
    * save proggress commit
    
    * MOTECH-2573: Add basic authentication to ihe-interop module
    
    *     MOTECH-2573
    
    Add basic authentication to ihe-interop module update
    
    * Tests added
    
    * MOTECH-2573: Remove unused imports
    
    * MOTECH-2573: Changed sending method
    
    * MOTECH-2573: Changed constructor order

--- ihe-interop/src/main/java/org/motechproject/ihe/interop/domain/HL7Recipient.java
+++ ihe-interop/src/main/java/org/motechproject/ihe/interop/domain/HL7Recipient.java
@@ -9,12 +9,22 @@ public class HL7Recipient {
 
     private String recipientUrl;
 
+    private String recipientUsername;
+
+    private String recipientPassword;
+
     public HL7Recipient() {
     }
 
     public HL7Recipient(String recipientName, String recipientUrl) {
+        this(recipientName, recipientUrl, null, null);
+    }
+
+    public HL7Recipient(String recipientName, String recipientUrl, String recipientUsername, String recipientPassword) {
         this.recipientName = recipientName;
         this.recipientUrl = recipientUrl;
+        this.recipientUsername = recipientUsername;
+        this.recipientPassword = recipientPassword;
     }
 
     public String getRecipientName() {
@@ -31,5 +41,21 @@ public class HL7Recipient {
 
     public void setRecipientUrl(String recipientUrl) {
         this.recipientUrl = recipientUrl;
+    }
+
+    public String getRecipientUsername() {
+        return recipientUsername;
+    }
+
+    public void setRecipientUsername(String recipientUsername) {
+        this.recipientUsername = recipientUsername;
+    }
+
+    public String getRecipientPassword() {
+        return recipientPassword;
+    }
+
+    public void setRecipientPassword(String recipientPassword) {
+        this.recipientPassword = recipientPassword;
     }
 }
--- ihe-interop/src/main/java/org/motechproject/ihe/interop/service/impl/IHETemplateServiceImpl.java
+++ ihe-interop/src/main/java/org/motechproject/ihe/interop/service/impl/IHETemplateServiceImpl.java
@@ -1,9 +1,12 @@
 package org.motechproject.ihe.interop.service.impl;
 
-
+import org.apache.commons.httpclient.Credentials;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.motechproject.ihe.interop.domain.HL7Recipient;
 import org.motechproject.ihe.interop.service.IHETemplateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,11 +25,31 @@ public class IHETemplateServiceImpl implements IHETemplateService {
     @Override
     public void sendTemplateToRecipientUrl(String url, String template) throws IOException {
 
+        sendTemplate(url, template, null);
+    }
+
+    @Override
+    public void sendTemplateToRecipientUrlWithBasicAuthentication(HL7Recipient recipient, String template) throws IOException {
+
+        UsernamePasswordCredentials credentials = new UsernamePasswordCredentials(recipient.getRecipientUsername(),
+                recipient.getRecipientPassword());
+
+        sendTemplate(recipient.getRecipientUrl(), template, credentials);
+    }
+
+    private void sendTemplate(String url, String template, Credentials credentials) throws IOException {
+
         PostMethod post = new PostMethod(url);
-        post.setRequestEntity(new StringRequestEntity(template, "application/xml", "utf-8"));
         HttpClient client = new HttpClient();
 
-        LOGGER.info("Sending template to URL: {}", url);
+        post.setRequestEntity(new StringRequestEntity(template, "application/xml", "utf-8"));
+
+        if (credentials != null) {
+            client.getParams().setAuthenticationPreemptive(true);
+            client.getState().setCredentials(AuthScope.ANY, credentials);
+        }
+
+        LOGGER.info("Sending template to URL: {}", post.getURI().toString());
         int responseCode = client.executeMethod(post);
         LOGGER.info("Response code: {}", responseCode);
     }
