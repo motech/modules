e528f8d0146453f8df439861b893ac9053513730
jhopen <jhopen@soldevelo.com>
2016-07-22 16:10:57 +0200

    MOTECH-2502:Task action integration for HTTP-Agent module (#329)
    * MOTECH-2502:Task action integration for HTTP-Agent module
    
    * MOTECH-2502:Task action integration for HTTP-Agent module
    
    * MOTECH-2504:Task action integration for HTTP-Agent module

--- http-agent/src/main/java/org/motechproject/http/agent/factory/HttpComponentsClientHttpRequestFactoryWithAuth.java
+++ http-agent/src/main/java/org/motechproject/http/agent/factory/HttpComponentsClientHttpRequestFactoryWithAuth.java
@@ -1,12 +1,16 @@
 package org.motechproject.http.agent.factory;
 
+import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.protocol.ClientContext;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.motechproject.config.SettingsFacade;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
@@ -18,12 +22,15 @@ import java.net.URI;
  */
 public class HttpComponentsClientHttpRequestFactoryWithAuth extends HttpComponentsClientHttpRequestFactory {
 
+    private static final String HTTP_AUTH_PREEMPTIVE = "http.auth.preemptive";
     private final String username;
     private final String password;
+    private final SettingsFacade settings;
 
-    public HttpComponentsClientHttpRequestFactoryWithAuth(String username, String password) {
+    public HttpComponentsClientHttpRequestFactoryWithAuth(String username, String password, SettingsFacade settings) {
         this.username = username;
         this.password = password;
+        this.settings = settings;
     }
 
     /**
@@ -31,14 +38,22 @@ public class HttpComponentsClientHttpRequestFactoryWithAuth extends HttpComponen
      */
     @Override
     protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
-        return createHttpContext();
+        BasicHttpContext localContext = new BasicHttpContext();
+        if (Boolean.valueOf(settings.getProperty(HTTP_AUTH_PREEMPTIVE))) {
+            HttpHost targetHost = new HttpHost(uri.getHost(), uri.getPort(), uri.getScheme());
+            BasicScheme basicScheme = new BasicScheme();
+            BasicAuthCache authCache = new BasicAuthCache();
+            authCache.put(targetHost, basicScheme);
+            localContext.setAttribute(ClientContext.AUTH_CACHE, authCache);
+        }
+
+        return createHttpContext(localContext);
     }
 
-    private HttpContext createHttpContext() {
+    private HttpContext createHttpContext(BasicHttpContext localContext) {
         CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-        BasicHttpContext localContext = new BasicHttpContext();
         localContext.setAttribute(ClientContext.CREDS_PROVIDER, credentialsProvider);
 
         return localContext;
