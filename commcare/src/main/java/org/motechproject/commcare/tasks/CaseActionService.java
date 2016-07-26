38fbf20c12c098bfb7b4880563594405392497dc
Filip Gomułkiewicz <fgomulkiewicz@gmail.com>
2016-07-26 14:24:40 +0200

    MOTECH-2726 Fixed javadocs

--- commcare/src/main/java/org/motechproject/commcare/tasks/CaseActionService.java
+++ commcare/src/main/java/org/motechproject/commcare/tasks/CaseActionService.java
@@ -5,9 +5,31 @@ import org.motechproject.commcare.domain.CaseTask;
 import java.util.Map;
 
 /**
- * This service is responsible for handling "Create Case" and "Update Case" actions in tasks.
+ * This service is responsible for handling Commcare Case actions in tasks.
  */
 public interface CaseActionService {
+
+    /**
+     * Creates a Commcare Case, by sending Case XML to the Submission API on the Commcare server.
+     *
+     * @param configName  the name of the configuration used for connecting to CommcareHQ
+     * @param caseType  the case type of case on CommCareHQ
+     * @param ownerId   the ID of the owner
+     * @param caseName  the case name of case on CommCareHQ
+     * @param fieldValues   the params that will be passed to CommcareHQ with case information
+     * @return  an object representing the case information and case actions submitted as case xml
+     */
     CaseTask createCase(String configName, String caseType, String ownerId, String caseName, Map<String, Object> fieldValues);
+
+    /**
+     * Updates a Commcare Case, by sending Case XML to the Submission API on the Commcare server.
+     * The case ID is required to identify the case that is supposed to be updated
+     *
+     * @param configName  the name of the configuration used for connecting to CommcareHQ
+     * @param caseId  the id of the case on CommCareHQ
+     * @param ownerId   the ID of the owner
+     * @param closeCase  true to optionally close the case on CommCareHQ
+     * @param fieldValues   the params that will be passed to CommcareHQ with case information
+     */
     void updateCase(String configName, String caseId, String ownerId, Boolean closeCase, Map<String, Object> fieldValues);
 }
--- commcare/src/main/java/org/motechproject/commcare/tasks/imports/ImportFormActionService.java
+++ commcare/src/main/java/org/motechproject/commcare/tasks/imports/ImportFormActionService.java
@@ -3,8 +3,17 @@ package org.motechproject.commcare.tasks.imports;
 import org.joda.time.DateTime;
 
 /**
- * This service is responsible for handling "Import Forms" action in tasks.
+ * This service is responsible for handling import form actions in tasks.
  */
 public interface ImportFormActionService {
+
+    /**
+     * This will import commcare forms for specific configuration. All forms will be imported or
+     * a subset of forms based on a particular datetime range.
+     *
+     * @param configName  the name of the configuration used for connecting to CommcareHQ
+     * @param startDate  the beginning of a datetime range
+     * @param endDate  the finish of a datetime range
+     */
     void importForms(String configName, DateTime startDate, DateTime endDate);
 }
