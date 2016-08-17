3432b1db563a8f5241f57a8525ab3e7cc0f32919
Filip Gomułkiewicz <fgomulkiewicz@gmail.com>
2016-08-17 13:56:47 +0200

    MOTECH-2838 Added javadocs and removed unecessary comments

--- openmrs/src/main/java/org/motechproject/openmrs/service/OpenMRSCohortService.java
+++ openmrs/src/main/java/org/motechproject/openmrs/service/OpenMRSCohortService.java
@@ -4,7 +4,21 @@ import org.motechproject.openmrs.domain.CohortQueryReport;
 
 import java.util.Map;
 
+/**
+ * Service responsible for managing cohorts on the OpenMRS servers.
+ */
 public interface OpenMRSCohortService {
-    // MOTECH-2801: method should return some kind of collection (containing cohort members)
+
+    /**
+     * Gets the Cohort Query report from the OpenMRS server with given {@code parameters}.
+     * The Cohort Query is identified by UUID.
+     * Configuration with the given {@code configName} will be used while performing this action.
+     *
+     * @param configName  the name of the configuration
+     * @param cohortQueryUuid  the cohort query uuid
+     * @param parameters  the additional parameters
+     * @return the cohort query report
+     */
     CohortQueryReport getCohortQueryReport(String configName, String cohortQueryUuid, Map<String, String> parameters);
+
 }
