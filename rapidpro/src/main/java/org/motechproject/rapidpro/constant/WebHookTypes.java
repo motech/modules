1b0dff165291156c28b9152eef63b37e2c697173
Scott Kimball <scott.russell.kimball@gmail.com>
2016-08-22 11:20:56 -0700

    All web hooks except for custom web hooks can be parsed by the module

---
+++ rapidpro/src/main/java/org/motechproject/rapidpro/constant/WebHookTypes.java
@@ -0,0 +1,16 @@
+package org.motechproject.rapidpro.constant;
+
+public final class WebHookConstants {
+
+    public static final String WEB_HOOK_MO_SMS = "mo_sms";
+    public static final String MT_SENT = "mt_sent";
+    public static final String MT_DLVD = "mt_dlvd";
+    public static final String MO_MISS = "mo_miss";
+    public static final String MO_CALL = "mo_call";
+    public static final String MT_CALL = "mt_call";
+    public static final String MT_MISS = "mt_miss";
+    public static final String ALARM = "alarm";
+    public static final String FLOW = "flow";
+
+    private WebHookConstants() { }
+}
---
+++ rapidpro/src/main/java/org/motechproject/rapidpro/util/WebHookParser.java
@@ -0,0 +1,7 @@
+package org.motechproject.rapidpro.webhook;
+
+/**
+ * Created by scottkimball on 8/10/16.
+ */
+public class WebHookRequestParser {
+}
