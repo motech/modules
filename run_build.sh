5738e3201ceeda684698187e397d852e6e3356c1
mkruszynski <mkruszynski@soldevelo.com>
2016-09-22 09:03:39 +0200

    MOTECH-2922: Changed post-push build flow

--- run_build.sh
+++ run_build.sh
@@ -7,5 +7,12 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
         mvn clean install -PIT -U
     fi
 elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$DB" = "mysql" ]; then
+
+    mvn clean install -PIT -U
+
+    if [ "$?" -ne 0 ]; then
+        exit 1
+    fi
+
     mvn clean deploy --settings deploy-settings.xml -Dmaven.test.skip=true -U
 fi
