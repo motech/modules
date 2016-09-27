83e9f606ef332e6719c868b2d4fc8c2897c0c238
Michał Jażdżewski <mjazdzewski@soldevelo.com>
2016-09-27 15:11:44 +0200

    Changed status error handling to match similar cases in DHIS2 module

--- dhis2/src/main/java/org/motechproject/dhis2/rest/service/impl/DhisWebServiceImpl.java
+++ dhis2/src/main/java/org/motechproject/dhis2/rest/service/impl/DhisWebServiceImpl.java
@@ -416,8 +416,7 @@ public class DhisWebServiceImpl implements DhisWebService {
             String contentString = IOUtils.toString(content);
             status = new ObjectMapper().readValue(contentString, DhisStatusResponse.class);
             if (status.getStatus() == DhisStatus.ERROR) {
-                LOGGER.debug(String.format("DHIS2 status response error details: %s", contentString));
-                String msg = String.format("Error in DHIS2 status response.");
+                String msg = String.format("DHIS2 status response error details: %s", contentString);
                 statusMessageService.warn(msg, MODULE_NAME);
                 throw new DhisWebException(msg);
             }
