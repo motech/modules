4db6f2cb67819dac65331b5ccbdfabebae18a9e8
kmadej <kmadej@soldevelo.com>
2017-01-31 20:12:04 +0100

    MOTECH-3128: Fixed OpenMRS Id Generation data source
    Now it should properly return new id value (bump by one). Additionally I fixed creating of message exception in few places.

--- openmrs/src/main/java/org/motechproject/openmrs/resource/GeneratedIdentifierResource.java
+++ openmrs/src/main/java/org/motechproject/openmrs/resource/GeneratedIdentifierResource.java
@@ -17,4 +17,14 @@ public interface GeneratedIdentifierResource {
      * @return latest generated identifier
      */
     GeneratedIdentifier getGeneratedIdentifier(Config config, String sourceName);
+
+    /**
+     * Sets latest identifier {@code identifier} for Generator with given {@code sourceName}.
+     * Configuration with the given {@code configName} will be used while performing this action.
+     *
+     * @param config the configuration to be used while performing this action
+     * @param sourceName the source name of the ID Generator
+     * @param identifier identifier to set
+     */
+    void setLatestIdentifier(Config config, String sourceName, String identifier);
 }
